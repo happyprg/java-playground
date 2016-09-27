1aaf50315462dcca0e0b80aba9ef51cfa14d387d
jeehong.lee <jeehong.lee@linecorp.com>
2016-09-27 11:21:53 +0900

    strategy pattern sample added

--- src/main/java/me/design/pattern/StrategyPattern.java
+++ src/main/java/me/design/pattern/StrategyPattern.java
@@ -1,7 +1,16 @@
+/*
+ * Copyright (c) happyprg@gmail.com 2016. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 package me.design.pattern;
 
 public class StrategyPattern {
 
+    //    https://en.wikipedia.org/wiki/Strategy_pattern
     public static void main(String[] args) {
 
         LabelReportTypeStrategyTypeA strategyTypeA = new LabelReportTypeStrategyTypeA();
