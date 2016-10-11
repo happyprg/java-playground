344b30fe70efd1019f2ddfdc52583aa2a4cb1f77
I <jeehong.lee@linecorp.com>
2016-10-11 13:59:53 +0900

    execute around pattern sample added

--- src/test/java/me/design/pattern/method/reference/ExamTransactionTest.java
+++ src/test/java/me/design/pattern/method/reference/ExamTransactionTest.java
@@ -14,29 +14,59 @@ import static java.util.stream.Collectors.toList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ExamTransactionTest {
 
-    @Test
-    public void testReduce1() {
-        Trader raoul = new Trader("Raoul", "Cambridge");
-        Trader mario = new Trader("Mario", "Milan");
-        Trader alan = new Trader("Alan", "cambridge");
-        Trader brian = new Trader("Brian", "Cambridge");
+    private Trader raoul;
+    private Trader mario;
+    private Trader alan;
+    private Trader brian;
+    private List<Transaction> transactions;
 
-        List<Transaction> transactions = Arrays.asList(
+    @Before
+    public void before() {
+        raoul = new Trader("Raoul", "Cambridge");
+        mario = new Trader("Mario", "Milan");
+        alan = new Trader("Alan", "Cambridge");
+        brian = new Trader("Brian", "Cambridge");
+
+        transactions = Arrays.asList(
                 new Transaction(brian, 2011, 300),
                 new Transaction(raoul, 2012, 1000),
                 new Transaction(raoul, 2011, 400),
                 new Transaction(mario, 2012, 710),
                 new Transaction(mario, 2012, 700),
                 new Transaction(alan, 2012, 950));
+    }
 
+    @Test
+    public void test1() {
         transactions.stream()
                     .filter((Transaction t) -> t.getYear() == 2011)
-                    .sorted(comparing(Transaction::getValue)).collect(toList()).forEach
-                (System.out::println);
+                    .sorted(comparing(Transaction::getValue))
+                    .collect(toList())
+                    .forEach(System.out::println);
+    }
+
+    @Test
+    public void test2() {
+        transactions.stream().map((Transaction t) -> t.getTrader().getCity())
+                    .distinct()
+                    .collect(toList())
+                    .forEach(System.out::println);
+    }
+
+    @Test
+    public void test3() {
+        transactions.stream().map(t -> t.getTrader()).filter((t) -> t.getCity().equals("Cambridge"))
+                    .sorted(comparing(Trader::getName)).collect(toList()).forEach(System.out::println);
+    }
+
+    @Test
+    public void test4() {
+
     }
 
     private class Trader {
@@ -58,6 +88,13 @@ public class ExamTransactionTest {
             return city;
         }
 
+        @Override
+        public String toString() {
+            return "Trader{" +
+                   "name='" + name + '\'' +
+                   ", city='" + city + '\'' +
+                   '}';
+        }
     }
 
     private class Transaction {
@@ -72,7 +109,7 @@ public class ExamTransactionTest {
             this.value = value;
         }
 
-        public Trader getBrian() {
+        public Trader getTrader() {
             return brian;
         }
 
