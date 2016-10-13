820d842ab2d1f5ab62658409eb948b2140291f0b
I <jeehong.lee@linecorp.com>
2016-10-13 12:28:28 +0900

    execute around pattern sample added

---
+++ src/test/java/me/design/pattern/method/reference/CustomCollectionTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) happyprg@gmail.com 2016. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package me.design.pattern.method.reference;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.junit.Test;
+
+public class CustomCollectionTest {
+
+    @Test
+    public void test1() {
+
+    }
+
+    public static <A> List<A> takeWhile(List<A> list, Predicate<A> p) {
+
+        int i = 0;
+        for (A item : list) {
+            if (!p.test(item)) {
+                return list.subList(0, i);
+            }
+            i++;
+        }
+
+        return list;
+    }
+
+    public static boolean isPrime(List<Integer> primes, int candidate) {
+        int candidateRoot = (int) Math.sqrt((double) candidate);
+        return takeWhile(primes, i -> i <= candidateRoot).stream().noneMatch(p -> candidate % p == 0);
+    }
+}
